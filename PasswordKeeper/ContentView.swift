ff15c29bfa764655968d34ca9594a77a5953623b
Dana Alenezi <Dana-al-enezi@live.com>
2020-04-12 20:14:33 +0300

    Initial Commit

---
+++ PasswordKeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PasswordKeeper
+//
+//  Created by Dana Alenezi on 4/12/20.
+//  Copyright © 2020 Dana Alenezi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
